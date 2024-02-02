503b5ae05f76b6ecaf67177e62313670f6a2b925
micael305 <miicael305@gmail.com>
2024-02-02 10:31:40 -0300

    Actualicé las consultas SQL

--- Consultas SQL/SQLINSERTS.sql
+++ Consultas SQL/SQLINSERTS.sql
@@ -9,17 +9,17 @@ insert into NumeroCorrelativo(ultimoNumero,cantidadDigitos,gestion,fechaActualiz
 --(AQUI DEBES INCLUIR TUS PROPIAS CLAVES Y CRENDENCIALES)
 select * from Configuracion;
 insert into Configuracion(recurso,propiedad,valor) values
-('Servicio_Correo','correo','micaelrodriguez68@gmail.com'),
-('Servicio_Correo','clave','chusebgzwzsjzsdb'),
+('Servicio_Correo','correo',''),
+('Servicio_Correo','clave',''),
 ('Servicio_Correo','alias','MiTienda.com'),
 ('Servicio_Correo','host','smtp.gmail.com'),
 ('Servicio_Correo','puerto','587')
 
 insert into Configuracion(recurso,propiedad,valor) values
-('FireBase_Storage','email','micael@gmail.com'),
-('FireBase_Storage','clave','micael'),
-('FireBase_Storage','ruta','mitiendaonline-2f5c4.appspot.com'),
-('FireBase_Storage','api_key','AIzaSyBpX85iE38mI88ykWbLcYYt_6HzuN_gYFU'),
+('FireBase_Storage','email',''),
+('FireBase_Storage','clave',''),
+('FireBase_Storage','ruta',''),
+('FireBase_Storage','api_key',''),
 ('FireBase_Storage','carpeta_usuario','IMAGENES_USUARIO'),
 ('FireBase_Storage','carpeta_producto','IMAGENES_PRODUCTO'),
 ('FireBase_Storage','carpeta_logo','IMAGENES_LOGO')
