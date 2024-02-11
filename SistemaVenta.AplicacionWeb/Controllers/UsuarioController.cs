4d2cbd4f992771884242c39e13d35928dc4a43aa
micael305 <miicael305@gmail.com>
2024-02-11 13:05:44 -0300

    MVC parte 10 y 11:  parte 11 incompleta

--- SistemaVenta.AplicacionWeb/Controllers/UsuarioController.cs
+++ SistemaVenta.AplicacionWeb/Controllers/UsuarioController.cs
@@ -26,5 +26,85 @@ namespace SistemaVenta.AplicacionWeb.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> ListaRoles()
+        {
+            List<VMRol> vmListaRoles = _mapper.Map<List<VMRol>>(await _rolServicio.Lista());
+            return StatusCode(StatusCodes.Status200OK, vmListaRoles);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Lista()
+        {
+            List<VMUsuario> vmUsuarioLista = _mapper.Map<List<VMUsuario>>(await _usuarioServicio.Lista());
+            return StatusCode(StatusCodes.Status200OK, new { data = vmUsuarioLista });
+        }
+        [HttpPost]
+        public async Task<IActionResult> Crear([FromForm] string modelo)
+        {
+            GenericResponse<VMUsuario> gResponse = new GenericResponse<VMUsuario>();
+
+            try
+            {
+                VMUsuario vmUsuario = JsonConvert.DeserializeObject<VMUsuario>(modelo);
+
+                string urlPlantillaCorreo = $"{this.Request.Scheme}://{this.Request.Host}/Plantilla/EnviarClave?correo=[correo]&clave=[clave]";
+
+                Usuario usuario_creado = await _usuarioServicio.Crear(_mapper.Map<Usuario>(vmUsuario), urlPlantillaCorreo);
+
+                vmUsuario = _mapper.Map<VMUsuario>(usuario_creado);
+
+                gResponse.Estado = true;
+                gResponse.Objeto = vmUsuario;
+            }
+            catch (Exception ex)
+            {
+                gResponse.Estado = false;
+                gResponse.Mensaje = ex.Message;
+            }
+            return StatusCode(StatusCodes.Status200OK, gResponse);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Editar([FromForm] string modelo)
+        {
+            GenericResponse<VMUsuario> gResponse = new GenericResponse<VMUsuario>();
+
+            try
+            {
+                VMUsuario vmUsuario = JsonConvert.DeserializeObject<VMUsuario>(modelo);
+
+                Usuario usuario_editado = await _usuarioServicio.Editar(_mapper.Map<Usuario>(vmUsuario));
+
+                vmUsuario = _mapper.Map<VMUsuario>(usuario_editado);
+
+                gResponse.Estado = true;
+                gResponse.Objeto = vmUsuario;
+            }
+            catch (Exception ex)
+            {
+                gResponse.Estado = false;
+                gResponse.Mensaje = ex.Message;
+            }
+            return StatusCode(StatusCodes.Status200OK, gResponse);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Eliminar(int idUsuario)
+        {
+            GenericResponse<string> gResponse = new GenericResponse<string>();
+
+            try
+            {
+                gResponse.Estado = await _usuarioServicio.Eliminar(idUsuario);
+            }
+            catch (Exception ex)
+            {
+                gResponse.Estado = false;
+                gResponse.Mensaje = ex.Message;
+            }
+            return StatusCode(StatusCodes.Status200OK, gResponse);
+        }
     }
 }
+
---
+++ SistemaVenta.AplicacionWeb/wwwroot/js/vistas/Usuario_Index.js
@@ -0,0 +1,134 @@
+﻿
+
+const MODELO_BASE = { 
+    idUsuario: 0,
+    nombre: "",
+    correo: "",
+    telefono: "",
+    idRol: 0,
+    esActivo: 1
+}
+
+let tablaData;
+
+$(document).ready(function () { 
+    fetch("/Usuario/ListaRoles") 
+        .then(response => { 
+            return response.ok ? response.json() : Promise.reject(response);
+        })
+        .then(responseJson => { 
+            if (responseJson.length > 0) {
+                responseJson.forEach((item) => {
+                    $("#cboRol").append(
+                        $("<option>").val(item.idRol).text(item.descripcion)
+                    )
+                })
+            }
+        })
+    $('#tbdata').DataTable({ 
+        responsive: true, 
+        "ajax": {
+            "url": '/Usuario/Lista',
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "idUsuario", "visible": false, "serchable": false },
+            { "data": "nombre" },
+            { "data": "correo" },
+            { "data": "telefono" },
+            { "data": "nombreRol" },
+            {
+                "data": "esActivo", render: function (data) {
+                    if (data == 1)
+                        return `<span class="badge badge-info">Activo</span>`;
+                    else
+                        return `<span class="badge badge-danger">No activo</span>`;
+                }
+            },
+            {
+                "defaultContent": '<button class="btn btn-primary btn-editar btn-sm mr-2"><i class="fas fa-pencil-alt"></i></button>' +
+                    '<button class="btn btn-danger btn-eliminar btn-sm"><i class="fas fa-trash-alt"></i></button>',
+                "orderable": false,
+                "searchable": false,
+                "width": "80px"
+            }
+        ],
+        order: [[0, "desc"]],
+        dom: "Bfrtip",
+        buttons: [ 
+            {
+                text: 'Exportar Excel',
+                extend: 'excelHtml5',
+                title: '',
+                filename: 'Reporte Usuarios',
+                exportOptions: {
+                    columns: [2, 3, 4, 5, 6] 
+                }
+            }, 'pageLength'
+        ],
+        language: {
+            url: "https://cdn.datatables.net/plug-ins/1.11.5/i18n/es-ES.json"
+        },
+    });
+})
+
+
+function mostrarModal(modelo = MODELO_BASE) 
+{
+    $("#txtId").val(modelo.idUsuario)
+    $("#txtNombre").val(modelo.nombre)
+    $("#txtCorreo").val(modelo.correo)
+    $("#txtTelefono").val(modelo.telefono)
+    $("#cboRol").val(modelo.idRol == 0 ? $("#cboRol option:first").val() : modelo.idRol) 
+    $("#cboEstado").val(modelo.esActivo)
+    $("#modalData").modal("show") 
+}
+
+$("#btnNuevo").click(function () {
+    mostrarModal()
+})
+
+$("#btnGuardar").click(function () {
+
+    const inputs = $("input.input-validar").serializeArray();
+    const inputs_sin_valor = inputs.filter(item => item.value.trim() == "")
+    if (inputs_sin_valor.length > 0) {
+        const mensaje = `Debe completar el campo: "${inputs_sin_valor[0].name}"`;
+        toastr.warning("", mensaje)
+        $(`input[name="${inputs_sin_valor[0].name}"]`).focus()
+        return;
+    }
+
+    const modelo = structuredClone(MODELO_BASE);
+    modelo["idUsuario"] = parseInt($("#txtId").val())
+    modelo["nombre"] = $("#txtNombre").val()
+    modelo["correo"] = $("#txtCorreo").val()
+    modelo["telefono"] = $("#txtTelefono").val()
+    modelo["idRol"] = $("#cboRol").val()
+    modelo["esActivo"] = $("#cboEstado").val()
+
+    const formData = new FormData();
+
+    $("modalData").find("div.modal-content").LoadingOverlay("show");
+
+    if (modelo.idUsuario == 0) {
+        fetch("", {
+            method: "POST",
+            body: formData
+        })
+            .then(response => {
+                $("modalData").find("div.modal-content").LoadingOverlay("hide");
+                return response.ok ? response.json() : Promise.reject(response);
+            })
+            .then(responseJson => {
+                if (responseJson.estado) {
+                    tablaData.row.add(responseJson.objeto).draw(false)
+                    $("modalData").modal("hide")
+                    swal("Listo!", "El usuario fue creado", "success")
+                } else {
+                    swal("Lo sentimos", responseJson.mensaje, "error")
+                }
+            })
+    } 
+    })
