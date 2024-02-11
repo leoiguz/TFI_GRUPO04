40b104549f36b6ea59593d3ea2c214d92e0107b9
leandro iguzquiza <leandroiguzquiza@alu.frt.utn.edu.ar>
2024-02-10 21:22:33 -0300

    MVC : Controlador y Vistas de Marca, Categoria y Tipo de Talle

---
+++ SistemaVenta.AplicacionWeb/Controllers/TipoTalleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaVenta.AplicacionWeb.Controllers
+{
+    public class TipoTalleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
