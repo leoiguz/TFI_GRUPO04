9ea2fd10b43357cbe99016cef403ae4fdfd4abb1
micael305 <miicael305@gmail.com>
2024-02-14 11:53:18 -0300

    MVC parte 19: Incompleta

---
+++ SistemaVenta.BLL/Implementacion/TipoComprobanteService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SistemaVenta.BLL.Interfaces;
+using SistemaVenta.DAL.Interfaces;
+using SistemaVenta.Entity;
+
+namespace SistemaVenta.BLL.Implementacion
+{
+	public class TipoComprobanteService : ITipoComprobanteService
+	{
+		private readonly IGenericRepository<TipoComprobante> _repositorio;
+
+        public TipoComprobanteService(IGenericRepository<TipoComprobante> repositorio)
+        {
+			_repositorio = repositorio;
+        }
+        public async Task<List<TipoComprobante>> Lista()
+		{
+			IQueryable<TipoComprobante> querry = await _repositorio.Consultar();
+			return querry.ToList();
+		}
+	}
+}
---
+++ SistemaVenta.BLL/Implementacion/VentaService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using SistemaVenta.BLL.Interfaces;
+using SistemaVenta.DAL.Interfaces;
+using SistemaVenta.Entity;
+
+namespace SistemaVenta.BLL.Implementacion
+{
+	public class VentaService : IVentaService
+	{
+		public Task<Venta> Detalle(string numeroVenta)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<List<Venta>> Historial(string numeroVenta, string fechaInicio, string fechaFin)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<List<Articulo>> ObtenerArticulos(string busqueda)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Venta> Registrar(Venta entidad)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<List<DetalleVenta>> Reporte(string fechaInicio, string fechaFin)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
